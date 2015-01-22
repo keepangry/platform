76317dd109a94b8bb0f89e26fec4f4cce27ddbf4
Yang Sen <0@keepangry.com>
2015-01-22 18:16:44 +0800

    2

--- web_platform/analysis/views.py
+++ web_platform/analysis/views.py
@@ -8,12 +8,27 @@ import jieba
 from django.template import Context, loader
 import os
 
+
+def get_jieba_socket(content):
+    from socket import *
+    HOST='localhost'
+    PORT=2012
+    BUFSIZ=10240
+    ADDR=(HOST, PORT)
+    client=socket(AF_INET, SOCK_STREAM)
+    client.connect(ADDR)
+    client.send(content)
+    data=client.recv(BUFSIZ)
+    result = json.loads(data.decode('utf8'))
+    return result['data']
+
 # Create your views here.
 def segmentation(req):
     content = req.POST['content']
-    jieba_res = jieba.cut(content,cut_all=False)
+    client=get_jieba_socket(content.encode('utf8'))
+    #jieba_res = jieba.cut(content,cut_all=False)
+    #seg_result = " ".join(jieba_res)
     result = {"status":0, "data":"", "info":""}
-    seg_result = " ".join(jieba_res)
-    result['data'] = seg_result
+    result['data'] = client
 
     return HttpResponse(json.dumps(result), content_type="application/json")
---
+++ web_platform/service/jieba_daemon.py
@@ -0,0 +1,24 @@
+#-*- coding: utf-8 -*-
+from socket import *
+import jieba
+import json
+
+HOST='localhost'
+PORT=2012
+BUFSIZ=10240
+ADDR=(HOST, PORT)
+sock=socket(AF_INET, SOCK_STREAM)
+sock.bind(ADDR)
+
+sock.listen(5)
+while True:
+    tcpClientSock, addr=sock.accept()
+    try:
+        data=tcpClientSock.recv(BUFSIZ)
+        jieba_res = " ".join(jieba.cut(data,cut_all=False))
+        result = {"data":jieba_res}
+        tcpClientSock.send(json.dumps(result).encode("utf-8"))
+    except:
+        tcpClientSock.close()
+tcpClientSock.close()
+sock.close()
