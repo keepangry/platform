acb937f7b78864e765f86230c47a9fe59d47eb60
0@keepangry.com <0@keepangry.com>
2015-03-18 17:11:57 +0800

    python crf

--- service/coae2008-car/car_target_daemon.py
+++ service/coae2008-car/car_target_daemon.py
@@ -7,6 +7,7 @@ import json
 import os
 from func0317 import *
 import subprocess
+import CRFPP
 
 
 
@@ -19,6 +20,8 @@ car_word_jieba_path = BASE_PATH+'output/car_word_jieba'
 #加载领域词库，TODO，应该标注词库的词性，默认名词可以
 jieba.load_userdict(car_word_jieba_path)
 
+tagger = CRFPP.Tagger("-m "+MODEL_PATH+" -v 3 -n2")
+
 #\n 替换值，这个值应该是词库里的一个非常高频的词，目的作为分隔符
 NDEL='豢魧'
 
@@ -28,6 +31,26 @@ def jieba_cut(text):
     for w in words:
         seg_list.append([w.word, w.flag])
     return seg_list
+
+def crf_test(text):
+    tagger.clear()
+    seg_list = jieba_cut(text.strip())
+    for line in seg_list:
+        l =  ' '.join(line)+' 1'
+        #add不能接受unicode
+        tagger.add(l.encode('utf-8'))
+
+    size = tagger.size()
+    xsize = tagger.xsize()
+    ysize = tagger.ysize()
+
+    tagger.parse()
+
+    #tagger.y2(i)就是预测的结果
+    ret_data = []
+    for i in range(0, (size - 1)):
+        ret_data.append([tagger.x(i, 0),tagger.y2(i)])
+    return ret_data
 
 
 #文章内容生成crf格式数据
@@ -51,10 +74,8 @@ def get_crf_result(test_file):
     cmd = [CRF_TEST,'-m '+MODEL_PATH,test_file]
     retcode = os.popen(' '.join(cmd))
     result = retcode.read()
-    #print result
-    #print '--'*20
+    
     result = result.strip().split('\n')
-    #print result
 
     #二维列表，带标注结果 0为词，1为O表示不知道，B为评价对象
     ret_data = []
@@ -78,12 +99,15 @@ def main():
         tcpClientSock, addr=sock.accept()
         try:
             data=tcpClientSock.recv(BUFSIZ)
-            #jieba_res = " ".join(jieba.cut(data,cut_all=False))
 
-            test_data = generate_crf_data(data)
-            result = get_crf_result(test_data)
+            #文件为中间结果方式
+            #test_data = generate_crf_data(data)
+            #result = get_crf_result(test_data)
+
+            #python直接调用crf方式
+            result = crf_test(data)
+
             result = {"data":result,'status':0}
-            #result = {"data":data}
             tcpClientSock.send(json.dumps(result).encode("utf-8"))
         except:
             tcpClientSock.close()
