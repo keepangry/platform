214bba199eb51c5f82578ea8e6faee5ce518f5e8
0@keepangry.com <0@keepangry.com>
2015-03-22 16:12:34 +0800

    observe_car

--- web/web_platform/car_extraction/views.py
+++ web/web_platform/car_extraction/views.py
@@ -1,3 +1,4 @@
+#encoding=utf8
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import Context, loader
@@ -17,4 +18,39 @@ def opinion_target(req):
         'name':name,
         'title':line,
     })
+    return HttpResponse(template.render(context))
+
+def observe_car(req):
+    template = loader.get_template('observe_car.html')
+    crf_data_path = os.getcwd()+'/data/crf_data'
+
+    data = []
+    with open(crf_data_path) as f:
+        texts = f.read().strip().split('\n\n')
+
+    for text in texts:
+        content = []
+        for line in text.strip().split('\n'):
+            line_l = line.split('\t')
+            content.append([line_l[0],line_l[-1]])
+        data.append(content)
+
+
+    # for text in texts:
+    #     content = ""
+    #     for line in text.strip().split('\n'):
+    #         line_l = line.split('\t')
+    #         if line[-1]=="B":
+    #             content += '['+line_l[0]+'] '
+    #         else:
+    #             content += line_l[0]+' '
+    #     data.append(content)
+    
+    line = ""
+    context = Context({
+        'title':line,
+        'data':data,
+        #'data1':data1,
+    })
+
     return HttpResponse(template.render(context))
--- web/web_platform/urls.py
+++ web/web_platform/urls.py
@@ -8,6 +8,7 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^car_extraction/opinion_target/$', 'web_platform.car_extraction.views.opinion_target'),
+    url(r'^car_extraction/observe_car/$', 'web_platform.car_extraction.views.observe_car'),
     url(r'^$', 'web_platform.car_extraction.views.opinion_target'),
     url(r'^analysis/segmentation/$', 'web_platform.analysis.views.segmentation'),
 )
